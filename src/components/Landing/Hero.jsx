3289aaebdccaa06ba67bcf7f498ce66c767740f7
Suagrcothe <ifeanyivalentine82@gmail.com>
2024-01-16 21:25:22 +0100

    navbar & hero section

--- src/components/Landing/Hero.jsx
+++ src/components/Landing/Hero.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import "../assets/Landing.css";
 import landingData from "../data/LandingData";
+import heroImage from "../assets/hero.png"
 
 const Hero = () => {
   return (
-    <div>
+    <div className="hero-container">
       <div className="hero-text">
-        <h1>
-          {landingData.firstHeader} <span>{landingData.span}</span>
+        <h1 className="hero-text1">
+          {landingData.firstHeader}{" "}
+          <span className="hero-text-span">{landingData.span}</span>
         </h1>
         <p>{landingData.secondHeader}</p>
         <button>Get Started</button>
       </div>
-      <div className="hero-text"></div>
+      <div className="hero-image">
+        <img src={heroImage} alt="" />
+      </div>
     </div>
   );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { AccountBalance } from "@mui/icons-material";
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navContainer">
+      <ul className="navbar">
+        <li className="navLogo">
+          <AccountBalance />
+          <h1>roboAdvisor</h1>
+        </li>
+        <ul className="menuList">
+          <li>Cash</li>
+          <li>Bonds</li>
+          <li>Automated Investing</li>
+          <li>Stocks</li>
+          <li>Learn</li>
+        </ul>
+        <li className="navBtnConainer">
+          <button className="navBtn">Log in</button>
+          <button className="navBtn1">Get Started</button>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
